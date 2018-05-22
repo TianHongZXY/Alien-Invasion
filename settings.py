afcc53848c9637ae55d7f9e9bd55bd4cbfd28532
TianHongZXY <39355004+TianHongZXY@users.noreply.github.com>
2018-05-22 22:45:06 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,49 @@
+class Settings():
+	'''存储外星人入侵的所有设置的类'''
+
+	def __init__(self):
+		'''初始化游戏的设置'''
+		#屏幕设置
+		self.screen_width = 1000
+		self.screen_height =666
+		self.bg_color = (255,255,255)
+		
+		self.ship_limit = 3
+		#子弹设置
+		
+		#self.bullet_width = 300
+		#self.bullet_height = 15
+		#self.bullet_color = 0,50,220
+		self.bullets_allowed = 8
+		
+		
+
+		#加快游戏节奏
+		self.speedup_scale = 1.1
+		#外星人点数提高速度
+		self.score_scale = 1.5
+		self.initialize_dynamic_settings()
+
+
+
+	def initialize_dynamic_settings(self):
+		'''初始化随游戏进行而变化的设置'''
+		self.ship_speed_factor = 2.5
+		self.bullet_speed_factor = 2
+		#外星人设置
+		self.fleet_drop_speed = 10
+		self.alien_speed_factor = 1
+		#fleet_direction为1表示向右移，为-1表示向左移
+		self.fleet_direction = 1
+
+		#计分
+		self.alien_points = 50
+
+	def increase_speed(self):
+		'''提高速度设置'''
+		self.ship_speed_factor *= self.speedup_scale
+		self.bullet_speed_factor *= self.speedup_scale
+		self.alien_speed_factor *= self.speedup_scale
+		self.fleet_drop_speed += 1
+		self.alien_points = int(self.alien_points * self.score_scale)
+		#print(self.alien_points)
